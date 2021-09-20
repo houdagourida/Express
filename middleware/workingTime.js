ea315a7a2409a5de075267c6d77c1919355036b1
houdagourida <@gmail.com>
2021-09-20 15:31:54 +0100

    modification

--- middleware/workingTime.js
+++ middleware/workingTime.js
@@ -1,11 +1,13 @@
-const workinghours= (req,res)=>{
+const workinghours= (req,res,next)=>{
+    console.log(res)
     let date=new Date();
     let days=date.getDay();
     let hours=date.getHours();
-    if (days==0 || days==5 || hours<9 || hours<17){
+    if (days==0 || days==6 || hours<9 || hours<17){
         res.sendFile("closed.html",{root:__dirname});
         }else {
             next();
         }
-    }
-    module.export=workinghours;
+    };
+
+    module.exports=workinghours;
--- srever2.js
+++ srever2.js
@@ -1,16 +1,12 @@
 const express=require('express');
-const app=express();
-const isAuth=require("./middleware/auth")
 const path=require('path')
-const workinghours=require('./middleware/workingTime')
+const app=express();
+const workinghours=require('./middleware/workingTime.js')
+
 // app.use(isAuth())
-app.use(express.json())
+app.use(workinghours)
 app.use(express.static(path.join(__dirname, "view")));
-const users=[{name:"jihen" , email:"jihen@gmail.com"},{name:"houda" , email:"houda@gmail.com"},{name:"sarra" , email:"jihen@gmail.com"}]
-app.post("./",(req,res)=>{
-    console.log(req)
-    res.send({users:[...users,req,body]})
-})
+
 
 app.get('/',(req, res)=> res.status(404).sendFile("./view/index.html", {root:__dirname}))
 app.get('/about',(req, res)=> res.sendFile("./view/about.html", {root:__dirname}))
